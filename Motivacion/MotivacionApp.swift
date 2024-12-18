58a0174278d1231cf2ce8006129449e5084940f1
Joel Vargas Reynoso <76189923+joelvshimself@users.noreply.github.com>
2024-12-17 19:54:11 -0600

    Initial commit

---
+++ Motivacion/MotivacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivacionApp.swift
+//  Motivacion
+//
+//  Created by Joel Vargas on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MotivacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotivacionTests/MotivacionTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotivacionTests.swift
+//  MotivacionTests
+//
+//  Created by Joel Vargas on 17/12/24.
+//
+
+import Testing
+@testable import Motivacion
+
+struct MotivacionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
